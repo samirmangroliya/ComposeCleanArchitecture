fa1f8764b9653292ccfa515abbd6324024b211ab
Samir <samir.mangroliya@zoodmall.com>
2025-03-07 16:09:23 +0530

    modules added...

--- data/src/androidTest/java/com/samir/data/ExampleInstrumentedTest.kt
+++ data/src/androidTest/java/com/samir/data/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.samir.data", appContext.packageName)
+        assertEquals("com.samir.data.test", appContext.packageName)
     }
 }
--- domain/src/androidTest/java/com/samir/domain/ExampleInstrumentedTest.kt
+++ domain/src/androidTest/java/com/samir/domain/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.samir.domain", appContext.packageName)
+        assertEquals("com.samir.domain.test", appContext.packageName)
     }
 }
